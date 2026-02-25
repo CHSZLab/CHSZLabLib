145ae70b4fe3fdc342e9db87f05455202a2b0ea3
Christian Schulz <christian.schulz.phone@gmail.com>
2026-02-25 14:46:16 +0100

    fix: guard __gnu_cxx compat header with __cplusplus

--- cmake/compat_gnu_cxx.h
+++ cmake/compat_gnu_cxx.h
@@ -1,7 +1,9 @@
 /* Provide an empty __gnu_cxx namespace on non-GCC compilers (Apple Clang). */
 #ifndef COMPAT_GNU_CXX_H
 #define COMPAT_GNU_CXX_H
+#ifdef __cplusplus
 #if !defined(__GNUC__) || defined(__clang__)
 namespace __gnu_cxx {}
+#endif
 #endif
 #endif
