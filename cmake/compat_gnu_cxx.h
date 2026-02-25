b1341de99fcbc08291da43b6064254edfa7c69ea
Christian Schulz <christian.schulz.phone@gmail.com>
2026-02-25 14:41:46 +0100

    fix: provide empty __gnu_cxx namespace for Apple Clang

---
+++ cmake/compat_gnu_cxx.h
@@ -0,0 +1,7 @@
+/* Provide an empty __gnu_cxx namespace on non-GCC compilers (Apple Clang). */
+#ifndef COMPAT_GNU_CXX_H
+#define COMPAT_GNU_CXX_H
+#if !defined(__GNUC__) || defined(__clang__)
+namespace __gnu_cxx {}
+#endif
+#endif
